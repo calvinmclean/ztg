0070107ad4459b0df4a345ba459d1b9d2f3ca445
Calvin McLean <calvinlmc@gmail.com>
2025-12-29 18:41:12 -0700

    Implement GRPC peer for dice

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -10,6 +12,10 @@ import (
 	"time"
 
 	"ztg/dice"
+	protodice "ztg/proto/dice"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
@@ -19,7 +25,11 @@ func main() {
 	peerAddr := os.Getenv("PEER_ADDR")
 
 	if addr != "" && peerAddr != "" {
-		runHTTP(sides, addr, peerAddr)
+		if os.Getenv("MODE") == "grpc" {
+			runGRPC(sides, addr, peerAddr)
+		} else {
+			runHTTP(sides, addr, peerAddr)
+		}
 	} else {
 		runSimple(sides)
 	}
@@ -62,6 +72,75 @@ func runHTTP(sides uint8, addr, peerAddr string) {
 
 		fmt.Println(out)
 
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func runGRPC(sides uint8, addr, peerAddr string) {
+	conn, err := grpc.NewClient(
+		peerAddr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalf("failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := protodice.NewRollerServiceClient(conn)
+
+	peer, err := dice.NewGRPCPeer(client)
+	if err != nil {
+		fmt.Printf("[ERROR] Failed to create gRPC peer: %v\n", err)
+		os.Exit(1)
+	}
+
+	grpcServer := grpc.NewServer()
+	protodice.RegisterRollerServiceServer(grpcServer, peer)
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Printf("[ERROR] Failed to bind gRPC server: %v\n", err)
+		fmt.Printf("[DEBUG] [ERROR] Failed to bind gRPC server on %s. Error details: %v\n", addr, err)
+		return
+	}
+
+	go func() {
+		if err := grpcServer.Serve(listener); err != nil {
+			fmt.Printf("[ERROR] Failed to serve gRPC: %v\n", err)
+		}
+	}()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	d, err := dice.NewRoller(addr, sides, peer)
+	if err != nil {
+		fmt.Printf("[ERROR] Failed to create Roller: %v\n", err)
+		os.Exit(1)
+	}
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+
+	ctx, bigCancel := context.WithCancel(context.Background())
+	defer bigCancel()
+
+	go func() {
+		<-sigCh
+		bigCancel()
+		grpcServer.GracefulStop()
+	}()
+
+	for {
+		ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+		roll := d.Roll(ctx)
+		out, err := roll.Get(8)
+		cancel()
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(out)
 		time.Sleep(1 * time.Second)
 	}
 }
